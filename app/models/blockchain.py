e0c00d95c5135673de2abd402420e02d6abc26ec
neeraj puntambekar <neeraj666@gmail.com>
2022-03-14 21:38:38 +1100

    updated procfile"

--- app/models/blockchain.py
+++ app/models/blockchain.py
@@ -5,7 +5,7 @@ def getAQHeros(ownerAddress):
   rpc_server = 'https://api.harmony.one'
   w3 = Web3(Web3.HTTPProvider(rpc_server))
   mainQuesterAddr = "0xcCa755BB6193fA582a122D89100b92fF179745Fd";
-  mainQuesterABI = """""" + os.environ['abi'] + """"""
+  mainQuesterABI = """""" + ENV['abi'] + """"""
   
   contract_address = Web3.toChecksumAddress(mainQuesterAddr)
   contract = w3.eth.contract(contract_address, abi=mainQuesterABI)
